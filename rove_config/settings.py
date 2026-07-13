06bd79bb3d38104798a7ca57ae4746d5351090b7
Chinyeaka Prosper <chinyeakaprosper2006@gmail.com>
2026-07-13 20:08:15 +0100

    v1.3: Hardcoded ALLOWED_HOSTS for Render

--- rove_config/settings.py
+++ rove_config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG',) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['*']  # Allow all hosts for development; change in production
 
 
 # Application definition
